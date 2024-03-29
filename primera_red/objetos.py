ce1b54360080fd066681972036b59b59f19cf2a3
samuelsantanaperales <samuelssp_98@hotmail.com>
2024-03-29 01:14:45 -0500

    manos a la obra

---
+++ primera_red/objetos.py
@@ -0,0 +1,11 @@
+class persona():
+    def __init__(self, nombre, apellido, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+    def mostrar_detalle(self):
+        print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
+
+a1=persona('Juan', 'Perez', 28)
+print(a1)
